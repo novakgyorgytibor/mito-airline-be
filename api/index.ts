b4c6bfffff9e2fffeb445db377a85a42cc319d67
novakgyorgytibor <novak.gyorgy.tibor@gmail.com>
2025-07-15 00:02:37 +0200

    [feature] Available destinations

--- api/index.ts
+++ api/index.ts
@@ -19,6 +19,23 @@ app.get("/stations", (req: Request, res: Response) => {
     res.send(stations);
 });
 
+app.get("/available-destinations", (req: Request, res: Response) => {
+    console.log(req.query, stations.length);
+    const availableDestinations: any[] = [];
+
+    if(req?.query?.origin) {
+        stations.forEach((station: any):void => {
+            if(station.connections.includes(req?.query.origin)) {
+                availableDestinations.push(station);
+            }
+        })
+    } else {
+        res.status(400).send("Required parameter: origin");
+    }
+    res.send(availableDestinations);
+});
+
+
 app.listen(PORT);
 
 module.exports = app;
