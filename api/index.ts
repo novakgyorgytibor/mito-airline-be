3eb3af4db6a4947d99ac23ac5e3ff3232fade6e9
novakgyorgytibor <novak.gyorgy.tibor@gmail.com>
2025-07-17 16:14:42 +0200

    [feature] cors 5

--- api/index.ts
+++ api/index.ts
@@ -14,10 +14,10 @@ app.use(cors({credentials: true, origin: true}));
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', '*');
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
